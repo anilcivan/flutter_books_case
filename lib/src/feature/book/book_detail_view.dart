19d279375449e4a243c115be91ba6658a96cc93c
anil <anilcivan.hsp@gmail.com>
2024-02-10 16:23:26 +0300

    Api Changed Pagination Search

--- lib/src/feature/book/book_detail_view.dart
+++ lib/src/feature/book/book_detail_view.dart
@@ -20,9 +20,9 @@ class _BookDetailState extends State<BookDetail> {
 
   Future<void> getBookDetails() async {
     final response = await Dio()
-        .get("https://www.googleapis.com/books/v1/volumes/${widget.id}");
+        .get("https://api.itbook.store/1.0/books/${widget.id}");
 
-    if (response.statusCode == 200) {
+    if (response.statusCode == 200) {      
       setState(() {
         bookDetailModel = BookDetailModel.fromJson(response.data);
       });
@@ -37,8 +37,8 @@ class _BookDetailState extends State<BookDetail> {
         title: const Text("Book Detail View", style:  TextStyle(color: Colors.white),),
         backgroundColor: Colors.indigoAccent),
       body: Column(children: [
-          Text(bookDetailModel?.volumeInfo?.title ?? ""),
-          Image.network(bookDetailModel?.volumeInfo?.imageLinks?.thumbnail ??"", errorBuilder:(context, error, stackTrace) => Text(""),)
+          Text(bookDetailModel?.title ?? ""),
+          Image.network(bookDetailModel?.image ??"", errorBuilder:(context, error, stackTrace) => Text(""),)
         ],)
  
     );
--- lib/src/feature/book/book_view.dart
+++ lib/src/feature/book/book_view.dart
@@ -1,10 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 import 'package:book_project/src/feature/book/book_detail_view.dart';
 import 'package:book_project/src/feature/book/book_model.dart';
-import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
 
 class BookView extends StatefulWidget {
-  const BookView({super.key});
+  const BookView({Key? key}) : super(key: key);
 
   @override
   State<BookView> createState() => _BookViewState();
@@ -12,55 +12,149 @@ class BookView extends StatefulWidget {
 
 class _BookViewState extends State<BookView> {
   BookModel? bookModel;
+  TextEditingController _searchController =
+      TextEditingController(text: "google");
+  int _page = 1;
+  int _totalPage = 0;
 
   @override
   void initState() {
-    getBook();
     super.initState();
+    // Varsayılan arama sorgusu ile kitapları getir
+    getBook(_searchController.text);
   }
 
-  Future<void> getBook() async {
-    final response = await Dio().get(
-        "https://www.googleapis.com/books/v1/volumes?q=Tolkien&maxResults=20&startIndex=20&orderBy=relevance");
+  Future<void> onChanged(String value) async {
+    // Yeni arama sorgusu ile kitapları getir
+    setState(() {
+      _page = 1;
+      _totalPage = 0;
+    });
+    getBook(value);
+  }
 
-    if (response.statusCode == 200) {
+  Future<void> getBook(String query) async {
+    try {
+      final response = await Dio()
+          .get("https://api.itbook.store/1.0/search/$query?page=$_page");
+
+      print("https://api.itbook.store/1.0/search/$query?page=$_page");
+
+      if (response.statusCode == 200) {
+        setState(() {
+          // Başarılı yanıtı alınca kitap modelini güncelle
+          bookModel = BookModel.fromJson(response.data);
+          _totalPage = (int.parse(bookModel?.total ?? "0") / 10).ceil();
+        });
+      } else {
+        // Başarısız yanıt durumunda kitap modelini null yap
+        setState(() {
+          bookModel = null;
+        });
+      }
+    } catch (e) {
+      // Hata durumunda kitap modelini null yap
       setState(() {
-         bookModel = BookModel.fromJson(response.data);
+        bookModel = null;
       });
-     
+      print("Ağ hatası: $e");
     }
+  }
+
+  void _getNextPage() {
+    setState(() {
+      if (_page < _totalPage) {
+        _page += 1;
+      }
+    });
+    getBook(_searchController.text);
+  }
+
+  void _getPreviousPage() {
+    setState(() {
+      if (_page > 1) {
+        _page -= 1;
+      }
+    });
+    getBook(_searchController.text);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.purple,
-        title: const Text(
-          "Book List",
-          style: TextStyle(color: Colors.white),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(kToolbarHeight + 20),
+        child: AppBar(
+          backgroundColor: Colors.purple,
+          title: TextField(
+            controller: _searchController,
+            decoration: InputDecoration(
+              hintText: 'Search...',
+              hintStyle: TextStyle(color: Colors.white),
+            ),
+            style: TextStyle(color: Colors.white),
+            onChanged: onChanged,
+          ),
         ),
       ),
-
-      body: ListView.separated(
-        itemCount: bookModel?.items.length ?? 0,
-        itemBuilder: (context, index){
-        return InkWell(
-          onTap: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context)=> BookDetail(id: bookModel?.items[index].id ?? "")));
-          },
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Flexible(child: Text(bookModel?.items[index].volumeInfo.title ?? "", overflow: TextOverflow.ellipsis,)),
-                const Icon(Icons.chevron_right)
-              ],
+      body: bookModel == null
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.separated(
+              itemCount: bookModel!.books.length + 1,
+              itemBuilder: (context, index) {
+                if (index == bookModel!.books.length) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        ElevatedButton(
+                          onPressed: _getPreviousPage,
+                          child: Text("Previous"),
+                        ),
+                        Text(_page.toString() + "/" + _totalPage.toString()),
+                        ElevatedButton(
+                          onPressed: _getNextPage,
+                          child: Text("Next"),
+                        ),
+                      ],
+                    ),
+                  );
+                } else {
+                  final book = bookModel!.books[index];
+                  return InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              BookDetail(id: book?.isbn13 ?? ""),
+                        ),
+                      );
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Flexible(
+                            child: Text(
+                              book.title ?? "",
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          const Icon(Icons.chevron_right),
+                        ],
+                      ),
+                    ),
+                  );
+                }
+              },
+              separatorBuilder: (BuildContext context, int index) =>
+                  const Divider(),
             ),
-          ),
-        );
-      }, separatorBuilder: (BuildContext context, int index) { return const Divider(); },),
     );
   }
 }
